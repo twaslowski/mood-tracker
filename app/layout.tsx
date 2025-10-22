d5dbe72ca49e19995bad2a635e24902a662d2ce5
Tobias Waslowski <tobias.waslowski@otto-payments.de>
2025-10-22 19:58:12 +0200

    chore: remove sample and tutorial code

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "mood-tracker",
+  description:
+    "A simple mood tracking app built with Next.js, Tailwind CSS, and Supabase.",
 };
 
 const geistSans = Geist({
