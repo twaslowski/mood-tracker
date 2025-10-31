ca0c89cf40ad5e969e9619b00358845a7f7f6768
Tobias Waslowski <contact@twaslowski.com>
2025-10-31 15:01:09 +0100

    fix: remove unused server action; add environment variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,17 @@ resource "vercel_project" "mood_tracker" {
     repo = "twaslowski/mood-tracker"
   }
 }
+
+resource "vercel_project_environment_variable" "supabase_url" {
+  project_id = vercel_project.mood_tracker.id
+  key  = "NEXT_PUBLIC_SUPABASE_URL"
+  value = "https://iwegsqflyrbynymrvfqa.supabase.co"
+  target = ["production"]
+}
+
+resource "vercel_project_environment_variable" "supabase_publishable_key" {
+  project_id = vercel_project.mood_tracker.id
+  key  = "NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY"
+  value = "sb_publishable_LGBKaf-s3vfHy-HFtkvvJQ_1aJ0cR3Q"
+  target = ["production"]
+}
